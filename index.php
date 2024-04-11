078b7ee55459dc71b1cec53724a2daa9270bb374
Scarlett338 <scarlettliu.job@gmail.com>
2024-04-11 23:51:22 +0800

    1st commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Scarlett</title>
+
+    <!-- css -->
+    <link href="./css/reset.css" rel="stylesheet" />
+    <link href="./css/stylesheet.css" rel="stylesheet" />
+
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">    
+
+    <!-- slider css-->
+    <link rel="stylesheet" type="text/css" href="./lib/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="./lib/slick/slick-theme.css"/>
+
+    <!-- icon -->
+    <script src="https://kit.fontawesome.com/b64fefc893.js" crossorigin="anonymous"></script>
+
+    <!-- jQuery & slider js -->
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="./lib/slick/slick.min.js"></script>
+
+</head>
+<body>
+    <header>
+        <div class="nav-container">
+            <div class="logo-con">
+                <a href="/"><h1 class="logo">Scarlett</h1></a>
+            </div>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/#about">About</a>
+                <a href="/#contact-me">Contact Me</a>
+            </nav>
+            </div>
+    </header>
+
+    <!-- main content -->
+
+    <main>
+
+    </main>
+
+    <!-- end of main content -->
+
+    <footer>
+        <p>© <?=date('Y')?> by Scarlett</p>
+    </footer>
+</body>
+</html>
